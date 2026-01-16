0489fded3db53a48f4bbdfdce75b8aff3633c37b
howlborne <awaisjavedshaheen+github@gmail.com>
2026-01-16 17:40:36 +0800

    Improvement: Changed GTK font to JetBrains Mono Semi Bold

--- scripts/kde_gtk_nordic_config.sh
+++ scripts/kde_gtk_nordic_config.sh
@@ -8,4 +8,4 @@ cp -r ./Themes/configuration/gtk-3-4/.config/* $HOME/.config/
 cp -r ./Themes/configuration/gtk-3-4/.icons ./Themes/configuration/gtk-3-4/.gtkrc-2.0 $HOME/
 
 ln -sf /usr/share/themes/Nordic-bluish-accent-v40/assets $HOME/.config/
-ln -sf /usr/share/themes/Nordic-bluish-accent-v40/gtk-4.0/gtk.css /usr/share/themes/Nordic-bluish-accent-v40/gtk-4.0/gtk-dark.css $HOME/.config/gtk-4.0/
+ln -sf /usr/share/themes/Nordic-bluish-accent-v40/gtk-4.0/* $HOME/.config/gtk-4.0/
