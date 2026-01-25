451a0a7b4e89ec370113cb9e6f5c0997fe28622a
howlborne <awaisjavedshaheen+github@gmail.com>
2026-01-25 22:31:39 +0800

    initial GoldenHarvest GTK theme

--- scripts/golden_harvest.sh
+++ scripts/golden_harvest.sh
@@ -22,15 +22,15 @@ THEMES=$HOME/.local/share/archland/global-themes
     kwriteconfig6 --file kdeglobals --group Icons --key Theme GoldenHarvest
 
     # apply GoldenHarvest GTK theme
-#     crudini --set ~/.config/gtk-3.0/settings.ini Settings gtk-theme-name GoldenHarvest-v40
+    crudini --set ~/.config/gtk-3.0/settings.ini Settings gtk-theme-name GoldenHarvest-v40
     crudini --set ~/.config/gtk-3.0/settings.ini Settings gtk-icon-theme-name GoldenHarvest
     crudini --set ~/.config/gtk-3.0/settings.ini Settings gtk-cursor-theme-name GoldenHarvest-cursors
-#     crudini --set ~/.config/gtk-4.0/settings.ini Settings gtk-theme-name GoldenHarvest-v40
+    crudini --set ~/.config/gtk-4.0/settings.ini Settings gtk-theme-name GoldenHarvest-v40
     crudini --set ~/.config/gtk-4.0/settings.ini Settings gtk-icon-theme-name GoldenHarvest
     crudini --set ~/.config/gtk-4.0/settings.ini Settings gtk-cursor-theme-name GoldenHarvest-cursors
     gsettings set org.gnome.desktop.interface icon-theme "GoldenHarvest"
     gsettings set org.gnome.desktop.interface cursor-theme "GoldenHarvest-cursors"
-#     gsettings set org.gnome.desktop.interface gtk-theme 'GoldenHarvest-v40'
+    gsettings set org.gnome.desktop.interface gtk-theme 'GoldenHarvest-v40'
 
 # restart hyprland -> since we are using greetd auto-login, logging out actually restarts hyprland
 loginctl terminate-session $XDG_SESSION_ID
--- scripts/theme.sh
+++ scripts/theme.sh
@@ -52,15 +52,15 @@ if [[ "$SELECTED" -eq 2 ]]; then
     kwriteconfig6 --file kdeglobals --group Icons --key Theme GoldenHarvest
 
     # apply GoldenHarvest GTK theme
-#     crudini --set ~/.config/gtk-3.0/settings.ini Settings gtk-theme-name GoldenHarvest-v40
+    crudini --set ~/.config/gtk-3.0/settings.ini Settings gtk-theme-name GoldenHarvest-v40
     crudini --set ~/.config/gtk-3.0/settings.ini Settings gtk-icon-theme-name GoldenHarvest
     crudini --set ~/.config/gtk-3.0/settings.ini Settings gtk-cursor-theme-name GoldenHarvest-cursors
-#     crudini --set ~/.config/gtk-4.0/settings.ini Settings gtk-theme-name GoldenHarvest-v40
+    crudini --set ~/.config/gtk-4.0/settings.ini Settings gtk-theme-name GoldenHarvest-v40
     crudini --set ~/.config/gtk-4.0/settings.ini Settings gtk-icon-theme-name GoldenHarvest
     crudini --set ~/.config/gtk-4.0/settings.ini Settings gtk-cursor-theme-name GoldenHarvest-cursors
     gsettings set org.gnome.desktop.interface icon-theme "GoldenHarvest"
     gsettings set org.gnome.desktop.interface cursor-theme "GoldenHarvest-cursors"
-#     gsettings set org.gnome.desktop.interface gtk-theme 'GoldenHarvest-v40'
+    gsettings set org.gnome.desktop.interface gtk-theme 'GoldenHarvest-v40'
 else
     echo "Installing Nordveil..."
 
